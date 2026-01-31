f78b176ba35504d6c68c63b1175b37498eb13301
Sami Bchiri <samibchiri@gmail.com>
2026-01-31 21:43:16 +0100

    Working on bar alg invalid etc

--- Cases.jsx
+++
@@ -1,9 +0,0 @@
-import ollCaseSet from "../data/ollCaseSet";
-import pllCaseSet from "../data/pllCaseSet";
-import eollCaseSet from "../data/eollCaseSet";
-import ocllCaseSet from "../data/ocllCaseSet";
-import epllCaseSet from "../data/epllCaseSet";
-import cpllCaseSet from "../data/cpllCaseSet";
-import f2l1CaseSet from "../data/f2l1CaseSet";
-
-import CaseSetCard from "./common/cubing/caseSetCard";
